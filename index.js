a91394b0cbe032c2c9a980a81e4bcda7b32889a2
AyeshadevX <aishaasghar436@gmail.com>
2025-09-09 20:46:52 +0500

    program

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-function createRandomUser() {
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'asdfghjkl'
+});
+
+try{
+    connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+});
+}catch(err){
+    console.log(err);
+}
+
+let createRandomUser = () => {
   return {
     userId: faker.string.uuid(),
-    username: faker.internet.username(),   // ✅ fixed
+    username: faker.helpers.slugify(faker.person.firstName() + '-' + faker.person.lastName()),
     email: faker.internet.email(),
     avatar: faker.image.avatar(),
     password: faker.internet.password(),
   };
-}
+};
 
 console.log(createRandomUser());
