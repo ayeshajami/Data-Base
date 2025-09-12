41a010cc9a95124efbab0343f9cc554bbb8a291f
AyeshadevX <aishaasghar436@gmail.com>
2025-09-12 16:50:07 +0500

    pro

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -21,7 +21,7 @@ const userSchema=new mongoose.Schema({
 const User = mongoose.model("User",userSchema);
 
 
-User.findOneAndUpdate{name:"Sultan"},{age:40}).then((res)=>{
+User.findOneAndUpdate({name: "Sultan" } , {age:40}).then((res)=>{
     console.log(res);
 }).catch((err)=>{
     console.log(err);
