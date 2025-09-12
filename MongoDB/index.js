adb5bd269fa20c5a5bb93e12847fc43bf1cc695f
AyeshadevX <aishaasghar436@gmail.com>
2025-09-12 16:24:41 +0500

    program

---
+++ MongoDB/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+main()
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch(err => {
+    console.error(" MongoDB connection error:", err);
+  });
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+});
+const User = mongoose.model("User",userSchema);
+
+User.insertMany([
+    {name:"Amna",email:"amna@gmail.com",age:15},
+     {name:"Sara",email:"sara@gmail.com",age:23},
+      {name:"Ali",email:"ali@gmail.com",age:17},
+
+]).then((res)=>{
+    console.log(res);
+});
+
+
+
+// const user2= new User({
+// name:"Sultan",
+// email:"Sultan@gmail.com",
+// age:21,
+// });
+
+// user2.save().then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
