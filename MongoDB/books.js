aac493105a57ad2d1518123cc32beb5455c489b2
AyeshadevX <aishaasghar436@gmail.com>
2025-09-13 00:46:34 +0500

    pro

---
+++ MongoDB/books.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+main()
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch(err => {
+    console.error(" MongoDB connection error:", err);
+  });
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/amazone");
+}
+const bookSchema=new mongoose.Schema({
+    title:{
+        type:String,
+    },
+        
+    author:{
+        type:String,
+    },
+    price:{
+        type:Number,,
+    },
+});
