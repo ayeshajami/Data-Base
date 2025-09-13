33ae6a17f1d3f0e8a11d4f7f82180a60a9905679
AyeshadevX <aishaasghar436@gmail.com>
2025-09-13 23:33:49 +0500

    pro

--- MongoDB/books.js
+++ MongoDB/books.js
@@ -1,48 +1,48 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
-main()
-  .then(() => {
-    console.log("MongoDB connected!");
-  })
-  .catch(err => {
-    console.error(" MongoDB connection error:", err);
-  });
+// main()
+//   .then(() => {
+//     console.log("MongoDB connected!");
+//   })
+//   .catch(err => {
+//     console.error(" MongoDB connection error:", err);
+//   });
 
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/amazone");
-}
-const bookSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        maxLength:20,
-    },
+// async function main(){
+//     await mongoose.connect("mongodb://127.0.0.1:27017/amazone");
+// }
+// const bookSchema=new mongoose.Schema({
+//     title:{
+//         type:String,
+//         required:true,
+//         maxLength:20,
+//     },
         
-    author:{
-        type:String,
-    },
-    price:{
-        type:Number,
-        min:[50,"price is too low"],
-    },
-    discount:{
-        type:Number,
-        default:0,
-    },
-    category:{
-        type:String,
-        enum:["fiction","non-fiction"],
-    },
-    genre: [String],
+//     author:{
+//         type:String,
+//     },
+//     price:{
+//         type:Number,
+//         min:[50,"price is too low"],
+//     },
+//     discount:{
+//         type:Number,
+//         default:0,
+//     },
+//     category:{
+//         type:String,
+//         enum:["fiction","non-fiction"],
+//     },
+//     genre: [String],
 
-});
+// });
 
-const Book=mongoose.model("Book",bookSchema);
+// const Book=mongoose.model("Book",bookSchema);
 
-Book.findByIdAndUpdate("66c486771d031c1b3fb888e8",{price:100},{runValidators:true}).then((res)=>{console.log(res)}).catch((err)=>{
-    console.log(err.errors.price.properties.message);
-});
+// Book.findByIdAndUpdate("66c486771d031c1b3fb888e8",{price:100},{runValidators:true}).then((res)=>{console.log(res)}).catch((err)=>{
+//     console.log(err.errors.price.properties.message);
+// });
 //  let book1= new Book({
 //     title:"Mathematics",
 //     author:"RD Sharma",
@@ -59,3 +59,5 @@ Book.findByIdAndUpdate("66c486771d031c1b3fb888e8",{price:100},{runValidators:tru
 //   .catch((err) => {
 //     console.log(err);
 //   });
+
+
--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,62 +1,62 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
-main()
-  .then(() => {
-    console.log("MongoDB connected!");
-  })
-  .catch(err => {
-    console.error(" MongoDB connection error:", err);
-  });
+// main()
+//   .then(() => {
+//     console.log("MongoDB connected!");
+//   })
+//   .catch(err => {
+//     console.error(" MongoDB connection error:", err);
+//   });
 
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
-}
+// async function main(){
+//     await mongoose.connect("mongodb://127.0.0.1:27017/test");
+// }
 
-const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number,
-});
-const User = mongoose.model("User",userSchema);
-
-
-User.findByIdAndDelete("68c400bdd3e0519b4e83d4d9").then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log(err);
-});
-
-// User.findOneAndUpdate({name: "Sultan" } , {age:40},{new:true}).then((res)=>{
-//     console.log(res);
-// }).catch((err)=>{
-//     console.log(err);
+// const userSchema=new mongoose.Schema({
+//     name:String,
+//     email:String,
+//     age:Number,
 // });
-
-// User.find({age:{$gt:15}}).then((res)=>{
-//     console.log(res[0].name);
-// }).catch((err)=>{
-//     console.log(err);
-// });
-
-// User.insertMany([
-//     {name:"Amna",email:"amna@gmail.com",age:15},
-//      {name:"Sara",email:"sara@gmail.com",age:23},
-//       {name:"Ali",email:"ali@gmail.com",age:17},
-
-// ]).then((res)=>{
-//     console.log(res);
-// });
+// const User = mongoose.model("User",userSchema);
 
 
-// const user2= new User({
-// name:"Sultan",
-// email:"Sultan@gmail.com",
-// age:21,
-// });
+// // User.findByIdAndDelete("68c400bdd3e0519b4e83d4d9").then((res)=>{
+// //     console.log(res);
+// // }).catch((err)=>{
+// //     console.log(err);
+// // });
 
-// user2.save().then((res)=>{
-//     console.log(res);
-// }).catch((err)=>{
-//     console.log(err);
-// });
+// // User.findOneAndUpdate({name: "Sultan" } , {age:40},{new:true}).then((res)=>{
+// //     console.log(res);
+// // }).catch((err)=>{
+// //     console.log(err);
+// // });
+
+// // User.find({age:{$gt:15}}).then((res)=>{
+// //     console.log(res[0].name);
+// // }).catch((err)=>{
+// //     console.log(err);
+// // });
+
+// // User.insertMany([
+// //     {name:"Amna",email:"amna@gmail.com",age:15},
+// //      {name:"Sara",email:"sara@gmail.com",age:23},
+// //       {name:"Ali",email:"ali@gmail.com",age:17},
+
+// // ]).then((res)=>{
+// //     console.log(res);
+// // });
+
+
+// // const user2= new User({
+// // name:"Sultan",
+// // email:"Sultan@gmail.com",
+// // age:21,
+// // });
+
+// // user2.save().then((res)=>{
+// //     console.log(res);
+// // }).catch((err)=>{
+// //     console.log(err);
+// // });
