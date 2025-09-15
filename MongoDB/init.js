36f0cad14067c66fe071443aec33fa5ba46e1443
AyeshadevX <aishaasghar436@gmail.com>
2025-09-15 18:59:46 +0500

    pro

--- MongoDB/init.js
+++ MongoDB/init.js
@@ -9,14 +9,24 @@ main()
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
-
-let chat1=new Chat({
+let allChats=[
+    {
     from:"Aisha",
     to:"Amna",
     msg:"Send me your exam sheet",
     created_at:new Date(),
-});
+    },
+    { from:"Aisha",
+    to:"Amna",
+    msg:"Send me your exam sheet",
+    created_at:new Date(),
+    },
+    { from:"Aisha",
+    to:"Amna",
+    msg:"Send me your exam sheet",
+    created_at:new Date(),
+    },
+    
+  ];
+Chat.insertMany(allChats);
 
-chat1.save().then((res)=>{
-    console.log(res);
-});
