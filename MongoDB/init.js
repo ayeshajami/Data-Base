6599ab6b5fa82c2b538c65c0f809ba3dd800ef1b
AyeshadevX <aishaasghar436@gmail.com>
2025-09-15 18:54:14 +0500

    por

---
+++ MongoDB/init.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Chat=require("./models/chat.js");
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch((err)=>console.log(err));
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chat1=new Chat({
+    from:"Aisha",
+    to:"Amna",
+    msg:"Send me your exam sheet",
+    created_at:new Date(),
+});
+
+chat1.save().then((res)=>{
+    console.log(res);
+});
